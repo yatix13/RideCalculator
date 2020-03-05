30c8e8344933aa49aed5e0c32f52d35b879e5bbe
yatis <yatish.j.24@gmail.com>
2020-03-06 00:36:08 +0530

    commit

--- app/src/main/java/com/example/ridecalculator/CalculateActivity.java
+++ app/src/main/java/com/example/ridecalculator/CalculateActivity.java
@@ -1,12 +1,23 @@
 package com.example.ridecalculator;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DecimalFormat;
 
 public class CalculateActivity extends AppCompatActivity {
@@ -29,6 +40,8 @@ public class CalculateActivity extends AppCompatActivity {
         distance = Double.parseDouble(d.substring(0, d.length()-3));
         petrolPrice = 80.53;
         tv_distance.setText(d);
+        getJSON("https://polyphyodont-bets.000webhostapp.com/fetch_petrol_price.php");
+        Log.d("petrolprice",petrolPrice+"");
         tv_petrolprice.setText(petrolPrice+"");
 
     }
@@ -43,8 +56,99 @@ public class CalculateActivity extends AppCompatActivity {
             cost = Double.parseDouble(df.format(cost));
 
             tv_result.setText("Trip cost = ₹ "+cost);
+
+
         }
 
+    }
+
+    private void getJSON(final String urlWebService) {
+        /*
+         * As fetching the json string is a network operation
+         * And we cannot perform a network operation in main thread
+         * so we need an AsyncTask
+         * The constrains defined here are
+         * Void -> We are not passing anything
+         * Void -> Nothing at progress update as well
+         * String -> After completion it should return a string and it will be the json string
+         * */
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            //this method will be called before execution
+            //you can display a progress bar or something
+            //so that user can understand that he should wait
+            //as network operation may take some time
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            //this method will be called after execution
+            //so here we are displaying a toast with the json string
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Log.d("here",s);
+                try {
+                    JSONArray jsonArray = new JSONArray(s);
+                    for(int i=0; i<jsonArray.length();i++){
+
+                        JSONObject object = jsonArray.getJSONObject(i);
+                        String city = object.getString("city");
+                        if(city.equalsIgnoreCase("Mumbai")){
+
+                            petrolPrice = object.getDouble("price");
+                            break;
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader;
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        Log.d("json",json);
+                        sb.append(json + "\n");
+                    }
+
+
+                    //finally returning the read string
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
     }
 
 
